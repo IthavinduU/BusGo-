8acfde0c038c7efd515142a62ea0e1c81d152f02
IthavinduU <thavindul@gmail.com>
2023-08-17 21:19:45 +0530

    Bus Schedule update

--- app/src/main/java/com/example/busgo/ClientActivity.kt
+++ app/src/main/java/com/example/busgo/ClientActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+@Suppress("ControlFlowWithEmptyBody")
 class ClientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,9 @@ class ClientActivity : AppCompatActivity() {
 
         val searchAutoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.searchAutoCompleteTextView)
         val searchButton = findViewById<Button>(R.id.searchButton)
+        val busTypeRadioGroup = findViewById<RadioGroup>(R.id.busTypeRadioGroup)
+        val viewSchedulesButton = findViewById<Button>(R.id.viewSchedulesButton)
+        val scheduleTextView = findViewById<TextView>(R.id.scheduleTextView)
         val bookSeatsButton = findViewById<Button>(R.id.bookSeatsButton)
 
         val searchResults = listOf("Result 1", "Result 2", "Result 3")
@@ -25,9 +32,19 @@ class ClientActivity : AppCompatActivity() {
             val searchText = searchAutoCompleteTextView.text.toString()
 
             if (searchText.isNotEmpty()) {
-
+                // Implement your search logic here
             }
             searchAutoCompleteTextView.text.clear()
+        }
+
+        viewSchedulesButton.setOnClickListener {
+            val schedules = when (findViewById<RadioButton>(busTypeRadioGroup.checkedRadioButtonId).text.toString()) {
+                "Regular" -> "Regular Bus Schedules:\n- Morning: 8:00 AM\n- Afternoon: 1:00 PM\n- Evening: 6:00 PM"
+                "Luxury" -> "Luxury Bus Schedules:\n- Morning: 9:00 AM\n- Afternoon: 2:00 PM\n- Evening: 7:00 PM"
+                "Express" -> "Express Bus Schedules:\n- Morning: 7:30 AM\n- Afternoon: 12:30 PM\n- Evening: 5:30 PM"
+                else -> ""
+            }
+            scheduleTextView.text = schedules
         }
 
         bookSeatsButton.setOnClickListener {
